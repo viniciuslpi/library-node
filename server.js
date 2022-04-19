dfb8c49e8dbc45c02b856feaa8ea881fca382f35
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-19 17:09:57 -0300

    init server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const port = 3000;
 
+const rotas = {
+    '/': 'Curso de Node',
+    '/livros': 'Entrei na página '
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
     res.end('Curso de Node');
