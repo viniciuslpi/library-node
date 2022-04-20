b96fa9934dc4fcdee91ab94877bc60967a6040d5
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-20 12:01:28 -0300

    initializing server

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+    { id: 1, "titulo": "Senhor dos Aneis"},
+    { id: 2, "titulo": "O Hobbit" }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node')
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro foi cadastrado com sucesso!');
+})
+
+
+export default app;
