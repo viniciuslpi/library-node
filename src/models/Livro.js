474ff284610da5b39ef5e02e3d4349cc0f2aa39c
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2022-04-26 16:31:47 -0300

    versionKey disabled

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,10 +3,14 @@ import mongoose from "mongoose";
 const livroSchema = new mongoose.Schema({
     id: { type: String },
     titulo: { type: String, required: true },
-    autor:  { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true },
+    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true },
     editora: { type: String, required: true },
     numeroPaginas: { type: Number }
-});
+    },
+    {
+        versionKey: false
+    }
+);
 
 const livros = mongoose.model('livros', livroSchema);
 
